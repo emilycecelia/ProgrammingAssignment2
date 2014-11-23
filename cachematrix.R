1bbcfcccb34d26dcd15844c13640ab0e532d7b26
imjustalilbit <ecschumm@gmail.com>
2014-11-22 22:19:19 -0500

    First Submit
    Made comments, tested code.  Only works for 2x2 matrices?  Get a weird
    error otherwise: “Lapack routine dgesv: system is exactly singular:
    U[10,10] = 0 “ or some variation of U[10,10]

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## In combination, cacheSolve interacts with the object that results from 
+## makeCacheMatrix in order to avoid duplicating work
 
-## Write a short comment describing this function
+## the makeCacheMatrix function creates an object that stores attributes of a matrix, including
+## the matrix value itself.  It has inner functions that retrieve these attributes.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse of the matrix object, but first it checks the
+## object to see if it already has an inverse stored.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  inv <- solve(x$get())
+  x$setinverse(inv)
+  inv
 }
